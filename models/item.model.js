15465dcbc575f44d946312563aa2eba8a04538e2
ericthedev7 <ericthedev97@gmail.com>
2025-01-17 10:52:00 +0700

    testing date

--- models/item.model.js
+++ models/item.model.js
@@ -16,6 +16,10 @@ const itemSchema = new mongoose.Schema({
   photos: [{
     type: String
   }],
+  date: {
+    type: String,
+    default: ''
+  },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
