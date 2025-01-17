c1d43ab111de947766b10b2827627e3b774dd984
ericthedev7 <ericthedev97@gmail.com>
2025-01-17 23:53:32 +0700

    asdf

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -3,12 +3,10 @@ const itemService = require('../services/item.service');
 const { verifyToken } = require('../middleware/auth');
 const router = express.Router();
 
-router.use(verifyToken);
-
-router.post('/', itemService.create);
+router.post('/',verifyToken, itemService.create);
 router.get('/', itemService.findAll);
 router.get('/:id', itemService.findOne);
-router.put('/:id', itemService.update);
-router.delete('/:id', itemService.delete);
+router.put('/:id',verifyToken, itemService.update);
+router.delete('/:id',verifyToken, itemService.delete);
 
 module.exports = router;
